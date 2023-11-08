0080ed31714d569265201cd76be29f82ee4e4382
Eko Manurung <kukutukkaw@gmail.com>
2023-11-08 17:17:54 +0700

    add debugger (#8)
    * add debugger
    
    ---------
    
    Co-authored-by: Eko Manurung <eko.manurung@grabtaxi.com>

--- errors.go
+++ errors.go
@@ -1,10 +1,13 @@
 package common_plugin
 
+import "fmt"
+
 type Error struct {
 	Status int
 	Err    error
 }
 
 func (e Error) Error() string {
+	fmt.Println("only debugging purpose")
 	return e.Err.Error()
 }
