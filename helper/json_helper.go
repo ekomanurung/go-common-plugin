221b6a14f5125a3bdd4b35c218884a36af0b16b6
Eko Manurung <eko.manurung@grabtaxi.com>
2023-11-08 16:25:53 +0700

    add new ToString method

--- helper/json_helper.go
+++ helper/json_helper.go
@@ -15,3 +15,11 @@ func ToResponseBody(writer http.ResponseWriter, i interface{}) error {
 	encoder := json.NewEncoder(writer)
 	return encoder.Encode(i)
 }
+
+func ToString(i interface{}) (string, error) {
+	bytes, err := json.Marshal(i)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
